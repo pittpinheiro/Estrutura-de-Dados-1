6c6d2b51d4f924aaeda00284be391055875dde2d
Madu Pinheiro ⋆｡ﾟ👽🛸 ｡ <161403737+pittpinheiro@users.noreply.github.com>
2025-05-15 10:51:27 -0300

    Update atividadeED3.cpp

--- atividadeED3.cpp
+++ atividadeED3.cpp
@@ -25,3 +25,42 @@ int main(){
     cout<<Harshad(numero)<<endl;
     
 }
+
+// #include <iostream>
+// using namespace std;
+
+int fatorial(int n){
+    if (n==0)
+        return 1;
+    return n * fatorial(n-1);
+    
+}
+/*int main(){
+    int numero;
+    cout<<"Insira um valor: "<<endl;
+    cin>>numero;
+    cout<<fatorial(numero);
+}
+*/
+void imprimir(int v[], int tamanho){
+    for (int i = 0; i < tamanho; i++){
+        cout<< v[i]<<" ";
+    }
+    cout<<endl;
+}
+void fatorialVetor(int v[], int tamanho){
+    for (int i = 0; i < tamanho; i++){
+        v[i] = fatorial(v[i]);
+    }
+}
+
+int main(){
+    int tamanho;
+    cin>>tamanho;
+    int vetor[tamanho], i;
+    for (i= 0; i<tamanho; i++){
+        cin>>vetor[i];
+    }
+    imprimir(vetor,tamanho);
+    fatorialVetor(vetor,tamanho);
+}
